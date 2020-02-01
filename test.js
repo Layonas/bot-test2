be66e0557082a65931d5cea6bdc591f14bd29fa9
Dovydas Zilys <dovdaszl@gmail.com>
2020-02-01 14:49:54 +0200

    switched test

--- test.js
+++ test.js
@@ -89,8 +89,8 @@ bot.off('unready', ()=>
 {
     console.log('Bot is off');
 })
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
+var server_port =process.env.PORT || 80;
+var server_host ='0.0.0.0';
 server.listen(server_port, server_host, function() {
     console.log('Listening on port %d', server_port);
 });
