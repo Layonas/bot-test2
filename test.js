6b99f88044c8255e539c457c2581fcc1b3c77e55
Dovydas Zilys <dovdaszl@gmail.com>
2020-02-01 14:47:24 +0200

    changed listen again

--- test.js
+++ test.js
@@ -89,5 +89,9 @@ bot.off('unready', ()=>
 {
     console.log('Bot is off');
 })
-.listen(process.env.PORT);
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
 bot.login(token);
