24bf2a765c7b58c97c613cbe675654ca68decf60
Dovydas Zilys <dovdaszl@gmail.com>
2020-03-13 02:39:37 +0200

    fixed more bugs

--- functions/checking.js
+++ functions/checking.js
@@ -1,13 +1,13 @@
 module.exports = {
     name: 'checking',
     description: 'checks if the server is online',
-    execute(room, bot)
+   async execute(room, bot)
     {
         var a = 0;
         const ping = require('minecraft-server-util');
         const {Client, RichEmbed} = require('discord.js');
         
-        ping('0o0o0o0o0o0o0o0o.aternos.me', 25565, (error, response) =>{
+      await  ping('0o0o0o0o0o0o0o0o.aternos.me', 25565, (error, response) =>{
             if (error) throw error;
             if (response.version.slice(4) === 'Online')
             {
