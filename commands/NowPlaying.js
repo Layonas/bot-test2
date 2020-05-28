2502f7a047d9ffc68ba8f40cc2920a0e71792902
Dovydas Zilys <dovdaszl@gmail.com>
2020-05-28 18:29:34 +0300

    added timestamp for songs longer than 1 hour but less than 10

--- commands/NowPlaying.js
+++ commands/NowPlaying.js
@@ -3,6 +3,7 @@ module.exports = {
     description: 'Tells what is the song that is currently playing.',
     execute(msg, serverQueue){
         msg.channel.bulkDelete(1);
+        var Hours = new Date();
         var minutes = new Date();
         var seconds = new Date();
         var sekundes;
@@ -32,9 +33,23 @@ __Daina dar truks__ ${sekundes}s`);
             }
         }
 
+        sekundes = (serverQueue.songs[0].msgHours + serverQueue.songs[0].hours - Hours.getHours()) * 3600 + (serverQueue.songs[0].msgMinutes + serverQueue.songs[0].minutes - minutes.getMinutes()) * 60 + (serverQueue.songs[0].msgSeconds + serverQueue.songs[0].seconds - minutes.getSeconds()) ;
+        
+         if(sekundes > 3600){
+             min = sekundes / 60;
+            var h  = min / 60;
+            var ho = h.toString().slice(0,1);
+            min = min - ho*60;
+         }
+         if (min >= 10) var m = min.toString().slice(0,2);
+         else var m = min.slice(0,1); // eslint-disable-line
+
+         sekundes = sekundes - ho*3600 - m*60;
+
             return msg.reply(`Dabar groja **${serverQueue.songs[0].title}**
 __Dainos nuoroda__ <${serverQueue.songs[0].url}>
-__Dainos ilgis__ ${serverQueue.songs[0].hours}h ${serverQueue.songs[0].minutes}min ${serverQueue.songs[0].seconds}s`);
+__Dainos ilgis__ ${serverQueue.songs[0].hours}h ${serverQueue.songs[0].minutes}min ${serverQueue.songs[0].seconds}s
+__Daina dar truks__ ${ho}h ${m}m ${sekundes}s`);
     
 }
 };
