b2b3322a6025cffe72ccbeccb37f32f24e760e37
Dovydas Zilys <dovdaszl@gmail.com>
2020-05-30 12:45:59 +0300

    deleted unrequired function

--- functions/setPlacing.js
+++
@@ -1,8 +0,0 @@
-const a = new Set();
-a.add('p');
-a.add('pl');
-module.exports = {
-    name: 'setPlacing',
-    description: 'Places sets that can be called by different names.',
-    playSet: a,
-};
