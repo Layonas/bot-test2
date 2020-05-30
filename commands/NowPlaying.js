b0bfa9247cbe6536be005fec92e5207795a3ee49
Dovydas Zilys <dovdaszl@gmail.com>
2020-05-30 11:04:27 +0300

    Simplified the song duration showing time

--- commands/NowPlaying.js
+++ commands/NowPlaying.js
@@ -8,41 +8,19 @@ module.exports = {
         var seconds = new Date();
         var sekundes;
         var min = 0;
+        var h = 0;
         if(!serverQueue) return msg.reply('Dabar niekas negroja!');
-        if((serverQueue.songs[0].msgMinutes+serverQueue.songs[0].minutes) > 60){
-            sekundes = ((serverQueue.songs[0].msgMinutes+serverQueue.songs[0].minutes) -60)*60 - minutes.getMinutes() * 60 + serverQueue.songs[0].msgSeconds + serverQueue.songs[0].seconds - seconds.getSeconds();
-        }else sekundes = (serverQueue.songs[0].msgMinutes+serverQueue.songs[0].minutes) *60 - minutes.getMinutes() * 60 + serverQueue.songs[0].msgSeconds + serverQueue.songs[0].seconds - seconds.getSeconds();
 
-        if(serverQueue.songs[0].hours === 0){
-                while(sekundes > 60){
-                    min++;
-                    sekundes = sekundes - 60;
-                }
-            return msg.reply(`Dabar groja **${serverQueue.songs[0].title}**
-__Dainos nuoroda__ <${serverQueue.songs[0].url}>
-__Dainos ilgis__ ${serverQueue.songs[0].minutes}min ${serverQueue.songs[0].seconds}s
-__Daina dar truks__ ${min}min ${sekundes}s`);
-        }
-
-        if(serverQueue.songs[0].hours === 0){
-            if(serverQueue.songs[0].minutes === 0){
-return msg.reply(`Dabar groja **${serverQueue.songs[0].title}**
-__Dainos nuoroda__ <${serverQueue.songs[0].url}>
-__Dainos ilgis__ ${serverQueue.songs[0].seconds}s
-__Daina dar truks__ ${sekundes}s`);
-            }
-        }
-
-        sekundes = (serverQueue.songs[0].msgHours + serverQueue.songs[0].hours - Hours.getHours()) * 3600 + (serverQueue.songs[0].msgMinutes + serverQueue.songs[0].minutes - minutes.getMinutes()) * 60 + (serverQueue.songs[0].msgSeconds + serverQueue.songs[0].seconds - minutes.getSeconds()) ;
-        
+        sekundes = (serverQueue.songs[0].msgHours + serverQueue.songs[0].hours - Hours.getHours()) * 3600 + (serverQueue.songs[0].msgMinutes + serverQueue.songs[0].minutes - minutes.getMinutes()) * 60 + (serverQueue.songs[0].msgSeconds + serverQueue.songs[0].seconds - seconds.getSeconds()) ;
+        var ho = 0;
          if(sekundes > 3600){
-             min = sekundes / 60;
-            var h  = min / 60;
-            var ho = h.toString().slice(0,1);
+            min = sekundes / 60;
+            h  = min / 60;
+            ho = h.toString().slice(0,1);
             min = min - ho*60;
          }
          if (min >= 10) var m = min.toString().slice(0,2);
-         else var m = min.slice(0,1); // eslint-disable-line
+         else var m = min.toString().slice(0,1); // eslint-disable-line
 
          sekundes = sekundes - ho*3600 - m*60;
 
