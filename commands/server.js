d4c0032b4555e4cd7208bc91847dccb59431e7b8
Dovydas Zilys <dovdaszl@gmail.com>
2020-07-10 20:22:05 +0300

    Changed what server it is monitoring and simplyfied it

--- commands/server.js
+++ commands/server.js
@@ -3,40 +3,27 @@ module.exports = {
     alias: ['server'],
     description: 'Mc server checker.',
     execute(msg, ping, RichEmbed){
-        if (msg.author.username !== 'AdvancingBot1')
-        {
-        ping ('0o0o0o0o0o0o0o0o.aternos.me', 25565, (error, response) =>
+        if (msg.author.username === 'AdvancingBot1') return;
+        
+        ping ('g09.rfox.cloud', 10249, (error, response) =>
         {
             if (error) throw error;
-            if (response.version.slice(4) === 'Online')
-            {
-            const Embed = new RichEmbed()
-                .setTitle('Server status')
-                .addField('Status', response.version.slice(4), true )
-                .addField('Online players', 'One or more:)', true)
-                .addField('Server IP', response.host)
-                .setColor(0x3AFF00 )
-                .setThumbnail(msg.author.avatarURL);
-                
 
-            msg.channel.send(Embed);
-            console.log(response);
-            }
-            else 
-            {
             const Embed = new RichEmbed()
                 .setTitle('Server status')
-                .addField('Status', response.version.slice(4), true )
+                .addField('Version', response.version, true )
                 .addField('Online players', response.onlinePlayers, true)
-                .addField('Server IP', response.host)
-                .setColor(0xFF2D00 )
+                .addField('Server IP', `${response.host}:${response.port}`)
+                .setColor(0x3AFF00)
                 .setThumbnail(msg.author.avatarURL);
                 
 
             msg.channel.send(Embed);
-            console.log(response);
-            }
+
         });
-        }
     }
 };
+    
+    
+
+
