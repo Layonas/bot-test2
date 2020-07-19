b747df4e454fb5ad2e4d7919878514ab14170511
Dovydas Zilys <dovdaszl@gmail.com>
2020-07-19 21:15:07 +0300

    New command to check how long a cooldown lasts for a specific user

---
+++ commands/CooldownCheck.js
@@ -0,0 +1,28 @@
+module.exports = {
+    name: 'CooldownCheck',
+    alias: ['cc', 'cooldowncheck', 'checkcooldown', 'checkc', 'ColldownCheck'],
+    execute(msg, args, CommandCooldown, OwnerID, Ctime){
+
+        const user = msg.mentions.users.first();
+        const member = msg.guild.member(user);
+        var hours = new Date().getHours();
+        var minutes = new Date().getMinutes();
+        var seconds = new Date().getSeconds();
+
+        if(msg.author.id === OwnerID){ 
+        if(!args[1]) return msg.reply(`Prašau pasirink kurį žmogu tikrinsi.`);
+
+        if(member) {
+            if(CommandCooldown.has(user.id)){
+                var time = 0;
+                for(var i = 0; i < Ctime.length; i++){
+                    if(Ctime[i].ID === user.id) time = Ctime[i].length - ((hours - Ctime[i].addHours) * 3600 + (minutes - Ctime[i].addMinutes) * 60 + (seconds - Ctime[i].addSeconds)); 
+                }
+                return msg.channel.send(`${args[1]} dar turi ${time}s laukti, kol galės rašyti.`);
+            }
+            else return msg.reply(`${args[1]} nėra sąraše.`); 
+        }
+        else return msg.reply(`${args[1]} nėra šio kanalo narys.`);
+        }
+    } 
+};
