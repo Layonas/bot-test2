af81f49099cf7d57d56684dfee633cd0270f248a
Dovydas Zilys <dovdaszl@gmail.com>
2020-07-20 17:43:43 +0300

    Added new variables to the list

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 module.exports = {
 YOUTUBE_API_KEY: 'AIzaSyCGvtwHyJ98pzhso9gELWOc5K-6FXCsoSs',
-token: 'NjcyODM2MzEwMTc1NzExMjcz.XkQjrQ.23HJKoAYX9Zojsbq7Abk0c8FhYg'
-}
+token: 'NjcyODM2MzEwMTc1NzExMjcz.XkQjrQ.23HJKoAYX9Zojsbq7Abk0c8FhYg',
+OwnerID: '279665080000315393',
+BotID: '672836310175711273',
+};
