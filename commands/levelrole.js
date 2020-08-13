28dd009063ea3b965c95f9ade70402f402270478
Dovydas Zilys <dovdaszl@gmail.com>
2020-08-13 21:48:07 +0300

    Updated the usage so that a role can be added with a level range

--- commands/levelrole.js
+++ commands/levelrole.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'levelRole',
     alias: ['lr', 'levelRole', 'levelrole', 'levelr'],
-    usage: '!<alias> <pavadinimas> <lygis>',
+    usage: '!<alias> <pavadinimas> <lygis> (optional) <lygis>',
     example: '!lr juodas_batas 2000',
     description: 'Suggests what roles users wants to add.',
     execute(msg, args, OwnerID){
@@ -15,7 +15,8 @@ module.exports = {
         var role = args[1].replace(/_/gi, ' ');
         msg.channel.send(`Ačiū už jūsų bendradarbiavimą, **${role}** pridėta peržiūrėjimui.`);
 
-        return msg.guild.members.get(OwnerID).send(`Naują rolę pasiūlė ***${msg.author.username}*** Rolės pavadinimas **${role}** Rolės lygis **${args[2]}**`);
-
+        if(!args[3]) return msg.guild.members.get(OwnerID).send(`Naują rolę pasiūlė ***${msg.author.username}*** Rolės pavadinimas **${role}** Rolės lygis **${args[2]}**`);
+        else if(!isNaN(args[3]))return msg.guild.members.get(OwnerID).send(`Naują rolę pasiūlė ***${msg.author.username}*** Rolės pavadinimas **${role}** Rolės lygis nuo **${args[2]}** iki **${args[3]}**`);
+        else msg.reply('Neteisingai suformuluota komanda!');
     }
 };
