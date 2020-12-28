74936f972f545f565b5fb5fd194a317c19b3ef82
Dovydas Zilys <dovdaszl@gmail.com>
2020-12-28 14:06:10 +0200

    Fixed msg.delete

--- test.js
+++ test.js
@@ -113,7 +113,7 @@ bot.on ('message', async msg=>
     let arg = msg.content.toLowerCase().split(" ");
     let args = msg.content.substring(prefix.length).split(" ");
 
-    if (CommandCooldown.has(msg.author.id)) return await msg.Delete(0);
+    if (CommandCooldown.has(msg.author.id)) return await msg.delete(1);
 
     await func.get('Statistics').execute(msg, args, BotID, stats, bot, CommandCooldown);
     await func.get('HandleCommands').execute(msg, args, BotID, CommandCooldown, commandFiles, queue, prefix, Ctime, ytdl, youtube, bot, ping, RichEmbed, holder, OwnerID);
