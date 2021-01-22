803b6f8e926a10d13334af300b8c60b7d6c0a517
Dovydas Zilys <dovdaszl@gmail.com>
2021-01-22 14:54:24 +0200

    if pilam send emoji

--- functions/hello.js
+++ functions/hello.js
@@ -3,7 +3,8 @@ module.exports ={
     alias: ['hello'],
     execute(msg){
         if (msg.content.toLowerCase() === 'hello') return msg.channel.send('Hey you should have a really nice day:).');
+        if(msg.content.toLowerCase() === 'pilam') return msg.channel.send(`${msg.guild.emojis.cache.find(e => e.name === 'pilam')}`);
         if(!msg.mentions.users.first()) return;
         if(msg.mentions.users.first().id === process.env.USER_BOT && msg.content.toLowerCase().includes('sw')) return msg.channel.send('Zdarowa ^^');
     }
- };
+};
