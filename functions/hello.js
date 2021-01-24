ed45d1ce1bf5bb019f746a3db71700e07efe9054
Dovydas Zilys <dovdaszl@gmail.com>
2021-01-24 22:46:23 +0200

    New emoji to send

--- functions/hello.js
+++ functions/hello.js
@@ -4,6 +4,7 @@ module.exports ={
     execute(msg){
         if (msg.content.toLowerCase() === 'hello') return msg.channel.send('Hey you should have a really nice day:).');
         if(msg.content.toLowerCase() === 'pilam') return msg.channel.send(`${msg.guild.emojis.cache.find(e => e.name === 'pilam')}`);
+        if(msg.content.toLowerCase() === 'kek') return msg.channel.send(`${msg.guild.emojis.cache.find(e => e.name === 'kekw')}`);
         if(!msg.mentions.users.first()) return;
         if(msg.mentions.users.first().id === process.env.USER_BOT && msg.content.toLowerCase().includes('sw')) return msg.channel.send('Zdarowa ^^');
     }
