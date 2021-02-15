83cb0e9b4aaa54db2363314f08271432c7ee067c
Dovydas Zilys <dovdaszl@gmail.com>
2021-02-15 12:58:03 +0200

    Updated the embed so it wouldnt error because it reached maximum body size to sending embed with limited items in it

--- commands/RoleChecker.js
+++ commands/RoleChecker.js
@@ -25,16 +25,28 @@ module.exports = {
         var Roles = [];
         Object.keys(Role_info).forEach(role => Roles.push(role));
 
-        let embed = new MessageEmbed()
-        .setThumbnail(msg.author.avatarURL())
-        .setTitle('Roles')
-        .setFooter('More roles can be added so you can suggest with !lr <name> <level>', msg.guild.members.cache.get(process.env.USER_BOT).user.avatarURL())
-        .setColor('RANDOM')
-        .setDescription(`${Roles.map((roles) => `**${Role_info[roles].name}** nuo **${Role_info[roles].min_level}** iki **${Role_info[roles].max_level}** lygio **${Role_info[roles].color}**`).join('\n')}`)
-        .setAuthor(msg.guild.members.cache.get(process.env.USER_OWNER).user.username, msg.guild.members.cache.get(process.env.USER_OWNER).user.avatarURL());
+        var count = Math.ceil(Roles.length/30);
 
+        await msg.reply(`There are currently ${count} pages.
+Choose which page you want to see:`);
 
-        msg.channel.send(embed);
+        const filter = message => message.author.id === msg.author.id && message.content > 0 && message.content <= count;
+
+        var response = await msg.channel.awaitMessages(filter, {max: 1, time: 10000});
+
+        if(!response) return;
+
+        let rolesHold = Roles.slice(0+30*(response.first().content-1), 30*response.first().content);
+
+            let embed = new MessageEmbed()
+            .setThumbnail(msg.author.avatarURL())
+            .setTitle('Roles')
+            .setFooter('More roles can be added so you can suggest with !lr <name> <level>', msg.guild.members.cache.get(process.env.USER_BOT).user.avatarURL())
+            .setColor('RANDOM')
+            .setDescription(`${rolesHold.map((roles, index) => `**${index+1}.** **${Role_info[roles].name}** nuo **${Role_info[roles].min_level}** iki **${Role_info[roles].max_level}** lygio **${Role_info[roles].color}**`).join('\n')}`)
+            .setAuthor(msg.guild.members.cache.get(process.env.USER_OWNER).user.username, msg.guild.members.cache.get(process.env.USER_OWNER).user.avatarURL());
+
+            await msg.channel.send(embed);
 
         return client.end();
     }
