a5c52283c659037d4f6e75eda3b10ab8c9ced59c
Dovydas Zilys <dovdaszl@gmail.com>
2021-06-14 17:41:08 +0300

    Updated function to log messages to separate from photos

--- functions/AttachmentSending.js
+++ functions/AttachmentSending.js
@@ -1,16 +1,30 @@
 module.exports = {
     name: 'Attachments',
-    description: 'Checks whether photos or videos are sent',
-    execute(bot, msg){
-        const channel = bot.guilds.cache.get('672837775569190922').channels.cache.get('718514256932372561');
+    description: 'Checks whether photos or videos are sent. Also logs chat.',
+    async execute(bot, msg){
+        const PhotoChannel = bot.guilds.cache.get('672837775569190922').channels.cache.get('718514256932372561');
+        const ChatChannel = bot.guilds.cache.get('672837775569190922').channels.cache.get('853982351535898634');
         if (msg.attachments.size > 0){
-            if(msg.attachments.forEach(files => {
-                if(files.url.toLowerCase().endsWith('.png') || files.url.toLowerCase().endsWith('.jpg') || files.url.toLowerCase().endsWith('.mp4') || files.url.toLowerCase().endsWith('.webm') || files.url.toLowerCase().endsWith('.jpeg') || files.url.toLowerCase().endsWith('.mp3')) {
+            if(msg.attachments.forEach(async files => {
+                if(files.url.toLowerCase().endsWith('.gif') || files.url.toLowerCase().endsWith('.png') || files.url.toLowerCase().endsWith('.jpg') || files.url.toLowerCase().endsWith('.mp4') || files.url.toLowerCase().endsWith('.webm') || files.url.toLowerCase().endsWith('.jpeg') || files.url.toLowerCase().endsWith('.mp3')) {
                     if(files.url.endsWith('test.png')) return;
-                    return channel.send(`**${msg.author.username}**  išsiuntė - ${msg.createdAt} __+3h in LT__
+                    await ChatChannel.send(files.url);
+                    return PhotoChannel.send(`**${msg.author.username}**  išsiuntė - ${msg.createdAt} __+3h in LT__
 ${files.url}`);
 }}));
         }
-    }
 
+        if(msg.author.id === process.env.USER_OWNER || msg.author.id === process.env.USER_BOT) return;
+        if(msg.content !== 0) await ChatChannel.send(msg);
+        ChatChannel.messages.fetch()
+        .then(messages => messages.each(m => {
+            console.log(Date.now() - m.createdAt.getTime());
+            if(Date.now()-m.createdAt.getTime()>= 10*60*1000)
+            {
+                m.delete();
+            }
+        }))
+        .catch(err => console.log('Something happened with message logging \n' + err));
+
+    }
 };
