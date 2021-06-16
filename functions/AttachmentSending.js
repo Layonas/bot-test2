ece3f6736e9434aade64ad15661540b45d89a082
Dovydas Zilys <dovdaszl@gmail.com>
2021-06-16 13:25:19 +0300

    Fixed error when trying to delete messages

--- functions/AttachmentSending.js
+++ functions/AttachmentSending.js
@@ -8,24 +8,25 @@ module.exports = {
             if(msg.attachments.forEach(async files => {
                 if(files.url.toLowerCase().endsWith('.gif') || files.url.toLowerCase().endsWith('.png') || files.url.toLowerCase().endsWith('.jpg') || files.url.toLowerCase().endsWith('.mp4') || files.url.toLowerCase().endsWith('.webm') || files.url.toLowerCase().endsWith('.jpeg') || files.url.toLowerCase().endsWith('.mp3')) {
                     if(files.url.endsWith('test.png')) return;
-                    await ChatChannel.send(`**${msg.author.username}**`+ 
+                    ChatChannel.send(`**${msg.author.username}**`+ 
                     files.url);
                     return PhotoChannel.send(`**${msg.author.username}**  išsiuntė - ${msg.createdAt} __+3h in LT__
 ${files.url}`);
 }}));
-        }
+}
+
+        setInterval(() => {
+            console.log(`Delete now`);
+            ChatChannel.messages.fetch({cache:true})
+            .then(messages => messages.each(m =>{
+                if(Date.now() - m.createdAt.getTime() >= 10*60*1000)
+                    m.delete({timeout: 100}).catch(err => console.log(`Hard to delete. \n` + err));
+            })).catch(err => console.log(`Error fetching messages. \n` + err));
+        }, 10*60*1000);
 
         if(msg.author.id === process.env.USER_OWNER || msg.author.id === process.env.USER_BOT) return;
-        if(msg.content !== 0) await ChatChannel.send(`**${msg.author.username} sent** `
-        +msg);
-        ChatChannel.messages.fetch()
-        .then(messages => messages.each(async m => {
-            if(Date.now()-m.createdAt.getTime()>= 10*60*1000)
-            {
-                await m.delete().catch(err => console.log('Hard to delete \n' + err));
-            }
-        }))
-        .catch(err => console.log('Something happened with message logging \n' + err));
+        if(msg.content !== 0) ChatChannel.send(`**${msg.author.username} sent** `
+        +msg.content).catch(err => console.log(`Error while logging a message, probably a spam.(await removed)\n`+err));
 
     }
 };
