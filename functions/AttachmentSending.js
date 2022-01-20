bac34a03b04279e80e1756e2ce7daba7627749a6
Dovydas Zilys <dovdaszl@gmail.com>
2022-01-21 00:28:18 +0200

    Changed from createdAt to createdTimestamp for more accurate time

--- functions/AttachmentSending.js
+++ functions/AttachmentSending.js
@@ -18,8 +18,7 @@ module.exports = {
             msg.attachments.forEach(async files => {
                 if(files.url.toLowerCase().endsWith('.gif') || files.url.toLowerCase().endsWith('.png') || files.url.toLowerCase().endsWith('.jpg') || files.url.toLowerCase().endsWith('.mp4') || files.url.toLowerCase().endsWith('.webm') || files.url.toLowerCase().endsWith('.jpeg') || files.url.toLowerCase().endsWith('.mp3')) {
                     if(files.url.endsWith('test.png')) return;
-                    PhotoChannel.send(`**${msg.author.username}**  išsiuntė - ${msg.createdAt} __+3h in LT__
-${files.url}`);
+                    PhotoChannel.send(`**${msg.author.username}**  išsiuntė - ${msg.createdTimestamp} ${files.url}`);
 
 }
 });
