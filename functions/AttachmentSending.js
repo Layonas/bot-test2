d31b95890380baf57fbeffe6a8cf14c6e61b438f
Dovydas Zilys <dovdaszl@gmail.com>
2022-01-21 00:01:25 +0200

    error was in host service all along ...

--- functions/AttachmentSending.js
+++ functions/AttachmentSending.js
@@ -13,12 +13,12 @@ module.exports = {
 
         if (msg.attachments.size > 0){
 
-            const PhotoChannel = bot.guilds.cache.get(process.env.LOG_GUILD).channels.cache.get(process.env.LOG_CHANNEL_AT);
+            const PhotoChannel = (await bot.guilds.fetch(process.env.LOG_GUILD)).channels.cache.get(process.env.LOG_CHANNEL_AT);
 
             msg.attachments.forEach(async files => {
                 if(files.url.toLowerCase().endsWith('.gif') || files.url.toLowerCase().endsWith('.png') || files.url.toLowerCase().endsWith('.jpg') || files.url.toLowerCase().endsWith('.mp4') || files.url.toLowerCase().endsWith('.webm') || files.url.toLowerCase().endsWith('.jpeg') || files.url.toLowerCase().endsWith('.mp3')) {
                     if(files.url.endsWith('test.png')) return;
-                    await PhotoChannel.send(`**${msg.author.username}**  išsiuntė - ${msg.createdAt} __+3h in LT__
+                    PhotoChannel.send(`**${msg.author.username}**  išsiuntė - ${msg.createdAt} __+3h in LT__
 ${files.url}`);
 
 }
